09c3a8d67e202332c177ab540322297ad489b8dd
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-08-27 22:43:27 -0300

    feat: inicio backend

---
+++ backend/src/main/java/com/distriapp/controller_backend/ControllerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.distriapp.controller_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerBackendApplication.class, args);
+	}
+
+}
