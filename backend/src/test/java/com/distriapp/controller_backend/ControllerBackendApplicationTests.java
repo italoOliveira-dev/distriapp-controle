e740c8fcb968a1455207a3df269a5dc63ecd6a85
Italo Oliveira <152231347+italoOliveira-dev@users.noreply.github.com>
2025-08-27 22:45:37 -0300

    feat: inicio backend (#1)

---
+++ backend/src/test/java/com/distriapp/controller_backend/ControllerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distriapp.controller_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
